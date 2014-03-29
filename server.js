95046e40de24e91b393d368f1be4d657b939619e
Brian Di Palma <offler@gmail.com>
2014-03-29 11:11:57 +0000

    Change server to use server package.

--- server.js
+++ server.js
@@ -1,20 +1,5 @@
 "use strict";
 
-var http = require("http"),
-	nodeStatic = require("node-static"),
-	serverOptions = {headers: {"Cache-Control": "no-cache, must-revalidate"}};
+var server = require("server");
 
-
-var staticServer = new nodeStatic.Server(".", serverOptions);
-
-http.createServer(clientRequestHandler).listen(8080);
-
-console.info("Connected server to port", 8080);
-
-function clientRequestHandler(request, response) {
-	request.addListener("end", requestEndedListener).resume();
-	
-	function requestEndedListener() {
-		staticServer.serve(request, response);
-	}
-}
+server(process.argv);
